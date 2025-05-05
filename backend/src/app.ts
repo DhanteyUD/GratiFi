0c834f67c9409d0916c1ae34dfa90c76f0edebcb
Clinton Otse <otseclinton60@gmail.com>
2025-05-05 15:38:27 +0100

    server

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import apiRoutes from "@/routes/api";
+
+dotenv.config();
+
+const app = express();
+app.use(cors(), express.json());
+// app.use("/api", apiRoutes);
+
+export default app
---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import app from "@/app";
+import { PrismaClient } from "@prisma/client";
+
+const port = process.env.PORT || 5001;
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$connect();
+  app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(() => prisma.$disconnect());
