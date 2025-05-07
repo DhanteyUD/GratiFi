4a5aae203cfbb61077e44f72fcc7a196b2e04ac0
Clinton Otse <otseclinton60@gmail.com>
2025-05-07 08:56:25 +0100

    fix

--- frontend/src/pages/landing/contents/Hero.tsx
+++ frontend/src/pages/landing/contents/Hero.tsx
@@ -2,8 +2,9 @@ function Hero() {
   return (
     <section className="w-full h-screen px-[7.5%] pt-[150px] pb-[50px]">
       <div className="w-full h-full flex flex-col justify-end rounded-[15px] p-5 border border-[red]">
-        <p className="bg-[pink]">Tips made Easy.....</p>
-        <h1></h1>
+        <p className="bg-[pink]">Tips should be simple</p>
+        <h1>Modern and fast tipping system</h1>
+        <p>All it takes is a QR code to turn every interaction into a tip</p>
       </div>
     </section>
   );
