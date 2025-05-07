bc1bd7e46301a15c047cf3a3991f1df326f62122
Clinton Otse <otseclinton60@gmail.com>
2025-05-07 07:35:08 +0100

    Hero Fix

--- frontend/src/pages/landing/contents/Hero.tsx
+++ frontend/src/pages/landing/contents/Hero.tsx
@@ -1,8 +1,8 @@
 function Hero() {
   return (
     <section className="w-full h-screen px-[7.5%] pt-[150px] pb-[50px]">
-      <div className="w-full h-full rounded-[15px] p-5 border border-[red]">
-        hero
+      <div className="w-full h-full flex flex-col justify-end rounded-[15px] p-5 border border-[red]">
+        <p className="bg-[pink]">Tips made Easy.....</p>
       </div>
     </section>
   );
