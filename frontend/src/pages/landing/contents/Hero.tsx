bcd74eba87d54d6ec7859b85411a47b5e966b860
Clinton Otse <otseclinton60@gmail.com>
2025-05-07 08:12:51 +0100

    PWA fixed

--- frontend/src/pages/landing/contents/Hero.tsx
+++ frontend/src/pages/landing/contents/Hero.tsx
@@ -3,6 +3,7 @@ function Hero() {
     <section className="w-full h-screen px-[7.5%] pt-[150px] pb-[50px]">
       <div className="w-full h-full flex flex-col justify-end rounded-[15px] p-5 border border-[red]">
         <p className="bg-[pink]">Tips made Easy.....</p>
+        <h1></h1>
       </div>
     </section>
   );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,46 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "GratiFi",
+        short_name: "GratiFi",
+        description: "Send micro-tips with a single click!",
+        theme_color: "#ab9ff2",
+        background_color: "#f4f2f4",
+        icons: [
+          {
+            src: "/icons/icon-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
