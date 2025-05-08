7aabe3091dafac6cb8caa66d35261b635a44e4e6
Clinton Otse <otseclinton60@gmail.com>
2025-05-08 04:04:27 +0100

    login animation

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import AppLayout from "@/layout/AppLayout";
 
 function Login() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const variants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <AppLayout path={[]} canLogin={true} canSignup={true}>
+      <motion.section
+        ref={ref}
+        initial="hidden"
+        variants={variants}
+        animate={isInView ? "visible" : "hidden"}
+        transition={{ duration: 1, ease: "easeOut" }}
+      >
         <div>login</div>
+      </motion.section>
     </AppLayout>
   );
 }
