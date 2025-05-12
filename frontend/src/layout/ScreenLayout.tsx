909ae09728c4480c2d973541e42a8ff2fc6cb641
Clinton Otse <otseclinton60@gmail.com>
2025-05-13 00:40:53 +0100

    Screen layout fixed

--- frontend/src/layout/ScreenLayout.tsx
+++ frontend/src/layout/ScreenLayout.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   ScreenHeader,
@@ -5,7 +6,10 @@ import {
   MobileActionNMenu,
   HamburgerToggle,
 } from "@/components";
-import { sharedMenuItems } from "@/routes/path";
+import { getScreenMenuItems } from "@/routes/path";
+import { CivicAuthProvider } from "@civic/auth/react";
+import { configKeys } from "@/config";
+import { PanelLeftClose } from "lucide-react";
 import UseScreenSize from "@/hooks/UseScreenSize";
 import GratiFiLogo from "@/assets/image/gratifi-logo.png";
 import clsx from "clsx";
@@ -21,45 +25,97 @@ function ScreenLayout({
 }) {
   const { md } = UseScreenSize();
   const navigate = useNavigate();
+  const [isSideNavCollapsed, setIsSideNavCollapsed] = useState(false);
+  const [userToggled, setUserToggled] = useState(false);
+  const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+
+  // Manual toggle
+  const handleToggleSidebar = () => {
+    setIsSideNavCollapsed((prev) => !prev);
+    setUserToggled(true);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      setCurrentWidth(screenWidth);
+
+      // Only auto-toggle if user hasn't manually toggled
+      if (!userToggled) {
+        setIsSideNavCollapsed(screenWidth <= 1000);
+      }
+
+      // Reset manual toggle when moving across threshold
+      if (
+        (screenWidth > 1000 && currentWidth <= 1000) ||
+        (screenWidth <= 1000 && currentWidth > 1000)
+      ) {
+        setUserToggled(false);
+      }
+    };
+
+    handleResize(); // Run once on mount
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [userToggled, currentWidth]);
 
   return (
-    <>
+    <CivicAuthProvider clientId={configKeys.clientId}>
       {/* {!md && getAnimatedCursor()} */}
       <div className="bg-background w-full h-screen flex p-0 md:p-5 gap-4">
         {/* {md && <HamburgerToggle menu={sharedMenuItems} />} */}
         {!md && (
-          <div className="bg-primary w-[20%] h-full border border-b-[20px] border-main py-8 px-[1.5rem] text-white flex flex-col items-center gap-16">
+          <div
+            className={clsx(
+              "relative bg-white h-full border border-b-[20px] border-main py-5 px-3 text-white flex flex-col items-center gap-5 transition-all duration-300 ease-in-out",
+              isSideNavCollapsed ? "w-[10%]" : "w-[20%]"
+            )}
+          >
             <div
-              className="flex justify-center items-center animated_cursor cursor-pointer gap-[10px"
+              className="flex justify-center items-center animated_cursor cursor-pointer gap-[10px]"
               onClick={() => navigate("/")}
             >
-              <img
-                alt="GratiFi logo"
-                src={GratiFiLogo}
-                className="w-[40px] h-[40px]"
-              />
-              <h1 className="text-white font-pacifico text-2xl font-normal">
+              <img alt="GratiFi logo" src={GratiFiLogo} className="h-10 w-10" />
+              <h1
+                className={clsx(
+                  "hidden lg:block text-main font-calSans text-[30px] transition-all duration-300 ease-in-out",
+                  isSideNavCollapsed && "!hidden"
+                )}
+              >
                 GratiFi
               </h1>
             </div>
 
-            {/* <SideNavMenu menu={sharedMenuItems} /> */}
+            <SideNavMenu
+              menu={getScreenMenuItems("GratiStar")}
+              isSideNavCollapsed={isSideNavCollapsed}
+            />
+            <PanelLeftClose
+              size={20}
+              onClick={handleToggleSidebar}
+              className={clsx(
+                "absolute top-2 left-2 text-primary hover:text-main cursor-pointer transition-all duration-300 ease-in-out",
+                isSideNavCollapsed && "scale-x-[-1]"
+              )}
+            />
+            <span className="absolute top-2 right-2 w-[10px] h-[10px] rounded-full bg-background border border-main" />
           </div>
         )}
 
         <div
           className={clsx(
-            "relative gap-[2.5rem] flex flex-col overflow-auto w-full md:w-[80%] mb-[83px] md:mb-0 p-0",
+            "relative gap-[2rem] flex flex-col overflow-auto w-full md:w-[80%] mb-[83px] md:mb-0 p-0  border border-[red]",
             layoutPadding ? "pt-0 pb-5 px-5" : "px-4"
           )}
         >
           <ScreenHeader goBack={goBack} layoutPadding={layoutPadding} />
-          <div className="p-2 h-auto">{children}</div>
+          <div className="p-1 h-auto">{children}</div>
         </div>
 
         {/* {md && <MobileActionNMenu menu={sharedMenuItems} />} */}
       </div>
-    </>
+    </CivicAuthProvider>
   );
 }
 
