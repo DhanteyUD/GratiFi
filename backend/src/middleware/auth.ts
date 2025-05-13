8f6711f1385cb216b9bcdf5a0e89864f4fc086f0
Clinton Otse <otseclinton60@gmail.com>
2025-05-13 14:25:13 +0100

    auth | backend

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -12,17 +12,19 @@ export const authenticate: RequestHandler = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({ message: "Unauthorized! No token provided" });
     return;
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, configKeys.jwtSecret);
+    const decoded = jwt.verify(token, configKeys.jwtSecret) as {
+      email: string;
+    };
     (req as any).user = decoded;
     next();
   } catch (err) {
-    res.status(403).json({ message: "Invalid token" });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,25 +1,51 @@
-import { Router, Request, Response } from "express";
-
-// Extend the Request interface to include the user property
 declare global {
   namespace Express {
     interface Request {
-      user?: any; 
+      user?: any;
     }
   }
 }
+import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 import { authenticate } from "@/middleware/auth";
 import { NextFunction } from "express";
 
 const router = Router();
+const prisma = new PrismaClient();
+
 router.get(
   "/profile",
   authenticate,
-  (req: Request, res: Response, next: NextFunction): void => {
-    res.json({
-      user: req.user || null,
-    });
-    next();
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const email = req.user?.email;
+
+      if (!email) {
+        res.status(400).json({ error: "Email missing from token" });
+        return;
+      }
+
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+      }
+
+      res.json({
+        message: "User fetched successfully",
+        user,
+      });
+
+      return;
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+
+      return;
+    }
   }
 );
 
