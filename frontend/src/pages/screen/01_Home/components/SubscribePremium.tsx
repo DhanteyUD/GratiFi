15a51441dcac49c4a68a1e36177173ef9e1a808c
Clinton Otse <otseclinton60@gmail.com>
2025-05-23 16:12:39 +0100

    Home dark mode fixed

--- frontend/src/pages/screen/01_Home/components/SubscribePremium.tsx
+++ frontend/src/pages/screen/01_Home/components/SubscribePremium.tsx
@@ -2,16 +2,16 @@ import { Crown } from "lucide-react";
 
 function SubscribePremium() {
   return (
-    <div className="flex flex-col items-start border border-gray-300 p-4 rounded-xl bg-white/50 h-auto gap-2">
+    <div className="flex flex-col items-start border border-gray-300 dark:border-gray-600 p-4 rounded-xl bg-white/50 dark:bg-dark3/50 h-auto gap-2">
       <h2 className="text-[20px] font-calSans font-[600] text-primary flex gap-2 items-center">
         Subscribe to Premium
         <Crown size={18} className="animate-pulse" />
       </h2>
-      <p className="text-main text-[16px]">
+      <p className="text-main dark:text-gray-400 text-[16px]">
         Enjoy exclusive features and tools with your Premium subscription.
       </p>
 
-      <button className="mt-1 bg-main hover:bg-primary text-white font-semibold px-5 py-1.5 rounded-full transition-all duration-300 ease-in-out cursor-not-allowed">
+      <button className="mt-1 bg-main dark:bg-main/50 hover:bg-primary dark:hover:bg-main/50 text-white dark:text-primary font-semibold px-5 py-1.5 rounded-full transition-all duration-300 ease-in-out cursor-not-allowed">
         Subscribe
       </button>
     </div>
--- frontend/src/pages/screen/01_Home/components/Tabs.tsx
+++ frontend/src/pages/screen/01_Home/components/Tabs.tsx
@@ -9,19 +9,19 @@ const tabOptions = ["For you", "Following"];
 
 export default function Tabs({ activeTab, onTabChange }: TabsProps) {
   return (
-    <div className="sticky -top-[1px] md:top-0 flex w-full border-b border-gray-300 gap-5 bg-background z-[2]">
+    <div className="sticky -top-[1px] md:top-0 flex w-full border-b border-gray-300 dark:border-gray-600 gap-5 bg-background dark:bg-backgroundDark z-[2]">
       {tabOptions.map((tab) => (
         <button
           key={tab}
           onClick={() => onTabChange(tab)}
-          className="flex group justify-center md:hover:bg-primaryHover/50 w-full"
+          className="flex group justify-center md:hover:bg-primaryHover/50 md:dark:hover:bg-main/50 w-full"
         >
           <p
             className={clsx(
               "p-1 md:pt-3 text-[14px] md:text-base font-medium transition-colors duration-300",
               activeTab === tab
-                ? "border-b-4 border-primary text-main w-full md:w-auto font-calSans"
-                : "border-b-4 border-transparent text-gray-400 md:group-hover:text-primary"
+                ? "border-b-4 border-primary text-main dark:text-primary w-full md:w-auto font-calSans"
+                : "border-b-4 border-transparent text-gray-400 dark:text-gray-600 md:group-hover:text-primary"
             )}
           >
             {tab}
