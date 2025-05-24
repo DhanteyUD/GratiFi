ae0dff0d18a2f2e76d92e86e50588980d916cf8d
Clinton Otse <otseclinton60@gmail.com>
2025-05-24 02:02:01 +0100

    fixed

--- frontend/src/pages/screen/08_Profile/index.tsx
+++ frontend/src/pages/screen/08_Profile/index.tsx
@@ -1,11 +1,5 @@
-import { ScreenFallback } from "@/components";
-
 function Profile() {
-  return (
-   <div>
-    {/* Twitter like profile screen */}
-   </div>
-  );
+  return <div>{/* Twitter like profile screen */}</div>;
 }
 
 export default Profile;
