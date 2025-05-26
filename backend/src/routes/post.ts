d5284801c3253c220640b5017fd46fefd8290ce4
Clinton Otse <otseclinton60@gmail.com>
2025-05-26 04:53:31 +0100

    Adds delete

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -5,6 +5,7 @@ import {
   getPosts,
   getMyPosts,
   getPostsByUserId,
+  deletePost
 } from "@/controllers/postController";
 import { authenticate } from "@/middleware/auth";
 
@@ -14,5 +15,6 @@ router.post("/create-post", authenticate, asyncHandler(createPost));
 router.get("/posts", authenticate, asyncHandler(getPosts));
 router.get("/posts/me", authenticate, asyncHandler(getMyPosts));
 router.get("/posts/user/:userId", authenticate, asyncHandler(getPostsByUserId));
+router.delete("/posts/:postId", authenticate, asyncHandler(deletePost));
 
 export default router;
